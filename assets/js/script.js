ceb8019dab861594569cef29306119ac25b60834
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2021-09-28 11:26:05 -0600

    add ability to add task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,29 @@
+// taskItemEl.className = "task-item";
+// var taskItemEl = document.createElement("li");
+// taskItemEl.textContent = "hello";
+// tasksToDoEl.appendChild(taskItemEl);
 
+var buttonEl = document.querySelector("#save-task"); 
+var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
-
-var buttonEl = document.querySelector("#save-task");
-var tasksToDoEl = document.querySelector("#tasks-to-do");
-
-buttonEl.addEventListener("click", createTaskHandler) = function() {
-  var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(listItemEl);
-});
+var createTaskHandler = function() { 
+  var listItemEl = document.createElement("li"); 
+  listItemEl.className = "task-item"; 
+  listItemEl.textContent = "This is a new task."; 
+  tasksToDoEl.appendChild(listItemEl); 
+  }; 
 
 buttonEl.addEventListener("click", createTaskHandler);
+// buttonEl.addEventListener("click", function() {
+ 
+// });
 
-var createTaskHandler = function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-  }
+// buttonEl.addEventListener("click", createTaskHandler);
+
+// var createTaskHandler = function() {
+//     var listItemEl = document.createElement("li");
+//     listItemEl.className = "task-item";
+//     listItemEl.textContent = "This is a new task.";
+//     tasksToDoEl.appendChild(listItemEl);
+//   }
 
